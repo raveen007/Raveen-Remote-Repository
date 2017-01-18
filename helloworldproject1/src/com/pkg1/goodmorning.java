7aa61fa298a2df2d38afd94a7446aa239680ed8f
Raveen <Raveen@dell-PC>
2017-01-19 01:30:31 +0530

    addded one more sysout

--- helloworldproject1/src/com/pkg1/goodmorning.java
+++ helloworldproject1/src/com/pkg1/goodmorning.java
@@ -5,6 +5,7 @@ public class goodmorning {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("creating local repository");
+System.out.println("added second chnage to commit");
 	}
 
 }
